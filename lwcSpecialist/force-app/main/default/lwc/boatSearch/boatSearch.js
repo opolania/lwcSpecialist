ca485598cc0294241cd6fbbf1f9d5408fe4fd97b
omar <omarpolania@gmail.com>
2021-07-04 14:40:21 -0500

    Step 4 challenge

---
+++ lwcSpecialist/force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -0,0 +1,16 @@
+// imports
+export default class BoatSearch extends LightningElement {
+  isLoading = false;
+
+  // Handles loading event
+  handleLoading() { }
+
+  // Handles done loading event
+  handleDoneLoading() { }
+
+  // Handles search boat event
+  // This custom event comes from the form
+  searchBoats(event) { }
+
+  createNewBoat() { }
+}
---
+++ lwcSpecialist/force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
@@ -0,0 +1,40 @@
+// imports
+import { publish, MessageContext } from 'lightning/messageService';
+import { LightningElement,wire  } from 'lwc';
+// import getBoatTypes from the BoatDataService => getBoatTypes method';
+import getBoatTypes from '@salesforce/apex/BoatDataService.getBoatTypes';
+export default class BoatSearchForm extends LightningElement {
+  selectedBoatTypeId = '';
+
+  // Private
+  error = undefined;
+  searchOptions;
+
+  // Wire a custom Apex method
+  @wire(getBoatTypes)
+  boatTypes({ error, data }) {
+    if (data) {
+      this.searchOptions = data.map(type => {
+        console.log(` label: ${type.Name},value: ${type.Id}`);
+        return { label: type.Name, value: type.Id };
+      });
+      this.searchOptions.unshift({ label: 'All Types', value: '' });
+    } else if (error) {
+      this.searchOptions = undefined;
+      this.error = error;
+    }
+
+  }
+
+  // Fires event that the search option has changed.
+  // passes boatTypeId (value of this.selectedBoatTypeId) in the detail
+  handleSearchOptionChange(event) {
+    console.log(` event.detail.value ${event.detail.value}`);
+    this.selectedBoatTypeId = event.detail.value;
+    // Create the const searchEvent
+    // searchEvent must be the new custom event search
+     const searchEvent = new CustomEvent('search',{boatTypeId :this.selectedBoatTypeId});
+
+     this.dispatchEvent(searchEvent);
+  }
+}
